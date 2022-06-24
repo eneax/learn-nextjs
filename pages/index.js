48263cbd25ae1fcce85c0536369d4b5393c9fc79
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-06-24 14:40:08 +0200

    Navigating with the "Link" Component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
+import Link from "next/link";
+
 const HomePage = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
       <h1>Home Page</h1>
     </div>
   );
