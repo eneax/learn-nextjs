83e7abd04e83e9a774ac427813172fa8e926a7cc
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-07-01 16:47:06 +0200

    Parse incoming req and execute server-side code

---
+++ pages/api/feedback.js
@@ -0,0 +1,36 @@
+import fs from "fs";
+import path from "path";
+
+const handler = (req, res) => {
+  if (req.method === "POST") {
+    const { email, text } = req.body;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text,
+    };
+
+    // get the feedback file path
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    // get existing data from feedback file and convert it to JSON
+    const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
+    // add new feedback to the existing data
+    data.push(newFeedback);
+    // convert the data back to a string
+    const newData = JSON.stringify(data);
+    // write the new data to the feedback file
+    fs.writeFileSync(filePath, newData);
+    // send a response to the client
+    res.status(201).json({
+      message: "Success!",
+      feedback: newFeedback,
+    });
+  } else {
+    res.status(200).json({
+      message: "It works!",
+    });
+  }
+};
+
+export default handler;
