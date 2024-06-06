703032139f1208a617a0022ac56b5a7e3d6a2601
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-06-06 14:52:10 +0200

    update layout and create new snippet creation page

--- 02-snippets/src/app/snippets/new/page.tsx
+++ 02-snippets/src/app/snippets/new/page.tsx
@@ -1,3 +1,35 @@
 export default function SnippetCreatePage() {
-  return <div>Create a snippet</div>;
+  return (
+    <form>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title" className="w-12">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <label htmlFor="code" className="w-12">
+            Code
+          </label>
+          <textarea
+            name="code"
+            id="code"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
 }
