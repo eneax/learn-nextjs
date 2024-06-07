f9cb0134c5517bb99af921fdff9d31b84fe68f3e
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-06-07 14:36:34 +0200

    add functionality to edit a snippet

--- 02-snippets/src/components/snippet-edit-form.tsx
+++ 02-snippets/src/components/snippet-edit-form.tsx
@@ -1,13 +1,23 @@
 "use client";
 
 import type { Snippet } from "@prisma/client";
+import { useState } from "react";
 import Editor from "@monaco-editor/react";
+import { editSnippet } from "@/actions";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState<string>(snippet.code);
+
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+
+  const editSnippetAction = editSnippet.bind(null, snippet.id, code);
+
   return (
     <div>
       <Editor
@@ -18,7 +28,13 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         options={{
           minimap: { enabled: false },
         }}
+        onChange={handleEditorChange}
       />
+      <form action={editSnippetAction}>
+        <button type="submit" className="mt-4 p-2 border rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
