199161fab34da5be7e12f23eef6b11f5082109ae
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-06-07 08:39:15 +0200

    add server action functionality to create a new snippet

--- 02-snippets/src/app/snippets/new/page.tsx
+++ 02-snippets/src/app/snippets/new/page.tsx
@@ -1,6 +1,30 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
 export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // This is a server action in Next.js
+    "use server";
+
+    // Check user input and make sure it's valid
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log({ snippet });
+
+    // Redirect home
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
