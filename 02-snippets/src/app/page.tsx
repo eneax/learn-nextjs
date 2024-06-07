b9a488b33dff91199e938b43b279b78c4a4ea1a8
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-06-07 09:35:13 +0200

    update home page to display snippets as links

--- 02-snippets/src/app/page.tsx
+++ 02-snippets/src/app/page.tsx
@@ -1,17 +1,31 @@
+import Link from "next/link";
+
 import { db } from "@/db";
 
 export default async function HomePage() {
   const snippets = await db.snippet.findMany();
+
   const renderedSnippets = snippets.map((snippet) => (
-    <div key={snippet.id}>
-      <h3>{snippet.title}</h3>
-    </div>
+    <Link
+      key={snippet.id}
+      href={`/snippets/${snippet.id}`}
+      className="flex justify-between items-center p-2 border rounded"
+    >
+      <div>{snippet.title}</div>
+      <div>View</div>
+    </Link>
   ));
 
   return (
-    <div>
-      <h1>Home Page</h1>
-      {renderedSnippets}
-    </div>
+    <main>
+      <div className="flex justify-between items-center my-2">
+        <h1 className="text-lg font-bold">Snippets</h1>
+        <Link href="/snippets/new" className="border rounded p-2">
+          New
+        </Link>
+      </div>
+
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </main>
   );
 }
