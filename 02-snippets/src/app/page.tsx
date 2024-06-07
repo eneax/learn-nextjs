cdef8b2108125f24c3e3953bf92c85e307687fdd
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-06-07 08:56:49 +0200

    fetch data with server components

--- 02-snippets/src/app/page.tsx
+++ 02-snippets/src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function HomePage() {
-  return <div>Home Page</div>;
+import { db } from "@/db";
+
+export default async function HomePage() {
+  const snippets = await db.snippet.findMany();
+  const renderedSnippets = snippets.map((snippet) => (
+    <div key={snippet.id}>
+      <h3>{snippet.title}</h3>
+      <pre>{snippet.code}</pre>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {renderedSnippets}
+    </div>
+  );
 }
