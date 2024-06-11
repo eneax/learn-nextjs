34f630b3937b04014f2664f2be4b3c2c9c16f5f0
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-06-11 11:00:23 +0200

    add revalidation of cache after snippet creation and deletion

--- 02-snippets/src/actions/index.ts
+++ 02-snippets/src/actions/index.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { db } from "@/db";
 
@@ -17,6 +18,7 @@ export async function deleteSnippet(id: number) {
     where: { id },
   });
 
+  revalidatePath("/");
   redirect("/");
 }
 
@@ -60,6 +62,8 @@ export async function createSnippet(
     }
   }
 
+  // Revalidate cache
+  revalidatePath("/");
   // Redirect home
   redirect("/");
 }
