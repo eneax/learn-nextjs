6d51927ddc13f04d569e190e9ee34965f3428181
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-06-13 16:13:57 +0200

    add form data to createTopic function

--- 03-discuss/src/actions/create-topic.ts
+++ 03-discuss/src/actions/create-topic.ts
@@ -1,5 +1,9 @@
 "use server";
 
-export async function createTopic() {
+export async function createTopic(formData: FormData) {
+  const name = formData.get("name");
+  const description = formData.get("description");
+
+  console.log({ name, description });
   // TODO: revalidate the homepage
 }
--- 03-discuss/src/components/topics/topic-create-form.tsx
+++ 03-discuss/src/components/topics/topic-create-form.tsx
@@ -20,8 +20,14 @@ export default function TopicCreateForm() {
         <form action={actions.createTopic}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
-            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+            />
             <Textarea
+              name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
