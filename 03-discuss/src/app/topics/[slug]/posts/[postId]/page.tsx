9f550c47fb28053751c1abff745cb0e11b3ed406
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-06-14 15:13:57 +0200

    add CommentCreateForm component for creating comments

--- 03-discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
+++ 03-discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,27 @@
-export default function PostShowPage() {
+import Link from "next/link";
+
+import PostShow from "@/components/posts/post-show";
+import CommentCreateForm from "@/components/comments/comment-create-form";
+
+import paths from "@/paths";
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
   return (
-    <div>
-      <h1>Post Show Page</h1>
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {"< "}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} />
     </div>
   );
 }
