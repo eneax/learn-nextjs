9c61276f245429434296b871dc05efc9e29b8138
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-06-14 14:21:06 +0200

    add form validation and error handling to PostCreateForm

--- 03-discuss/src/actions/create-post.ts
+++ 03-discuss/src/actions/create-post.ts
@@ -1,5 +1,53 @@
 "use server";
 
-export async function createPost() {
+import type { Post } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
+
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const title = formData.get("title");
+  const content = formData.get("content");
+  const result = createPostSchema.safeParse({ title, content });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be authenticated to create a post."],
+      },
+    };
+  }
+
+  return {
+    errors: {},
+  };
+
   // TODO: revalidate the topic show page
 }
--- 03-discuss/src/components/posts/post-create-form.tsx
+++ 03-discuss/src/components/posts/post-create-form.tsx
@@ -14,13 +14,17 @@ import * as actions from "@/actions";
 import FormButton from "@/components/common/form-button";
 
 export default function PostCreateForm() {
+  const [formState, action] = useFormState(actions.createPost, {
+    errors: {},
+  });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Post</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action="">
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Post</h3>
 
@@ -29,13 +33,22 @@ export default function PostCreateForm() {
               label="Title"
               labelPlacement="outside"
               placeholder="Title"
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(", ")}
             />
-            <Input
+            <Textarea
               name="content"
               label="Content"
               labelPlacement="outside"
               placeholder="Content"
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(", ")}
             />
+            {formState.errors._form && (
+              <div className="bg-red-200 border border-red-400 p-2 rounded">
+                {formState.errors._form.join(", ")}
+              </div>
+            )}
 
             <FormButton>Create Post</FormButton>
           </div>
